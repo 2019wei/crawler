1ec8fb070357a671293b7e3a130ab7ecb23903b7
weiweill <46859361+2019wei@users.noreply.github.com>
2019-02-14 22:10:43 +0800

    Add files via upload

---
+++ 歷年天然氣價格波動圖/hwhw3.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 14 20:53:50 2019
+
+@author: user
+"""
+import pymysql
+import matplotlib.pyplot as pt
+
+db=pymysql.connect("localhost","root","p1234567","HistoryGasPrice2")
+cursor=db.cursor()
+sql="Select * from HistoryGasPrice2"
+
+listMonth=[]
+list1=[]
+list2=[]
+list3=[]
+list4=[]
+cursor.execute(sql)
+results=cursor.fetchall()
+for row in results:
+        listMonth.append(row[0])
+        list1.append(row[1])
+        list2.append(row[2])
+        list3.append(row[3])
+        list4.append(row[4])
+        print("%s,%6.4f,%6.2f,%6.4f,%6.2f"%\
+              (row[0],row[1],row[2],row[3],row[4]))
+
+db.close()
+pt.figure(num=None,figsize=(6,6),dpi=168,facecolor='w',edgecolor='k')
+pt.plot(listMonth,list1,lw=2,label='Industrial user')
+pt.plot(listMonth,list2,lw=2,label='Gas company')
+pt.plot(listMonth,list3,lw=2,label='electricity symbiosis')
+pt.plot(listMonth,list4,lw=2,label='Direct family')
+pt.xlabel('month')
+pt.ylabel('dollars/cubic meter')
+pt.legend()
+pt.title("Python Data Science")
+pt.show()
---
+++ 歷年天然氣價格波動圖/hwhw4.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 14 21:26:42 2019
+
+@author: user
+"""
+
+from bs4 import BeautifulSoup
+import requests
+import pymysql
+
+conn=pymysql.connect('localhost','root','p1234567','historygasprice2')
+cursor = conn.cursor()
+
+url='https://web.cpc.com.tw/division/lngb/oil-more8.aspx'
+headers = {
+'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
+}
+
+res = requests.get(url,headers=headers)
+soup = BeautifulSoup(res.text,'html.parser')
+mydata= soup.find_all('span',attrs={'id':'Showtd'})
+myrow=mydata[0].find_all('tr')
+
+mylist=[]
+
+
+for r in myrow:
+    c= r.find_all('td')
+    if(len(c[0].text)>0 and len(c[1].text)>0) and \
+        (len(c[2].text)>0 and len(c[3].text)>0) \
+                    and (len(c[4].text)>0):
+        dataList = []
+        dataList.append(c[0].text)        
+        dataList.append(c[1].text)
+        dataList.append(c[2].text)
+        dataList.append(c[3].text)
+        dataList.append(c[4].text)
+        mylist.append(dataList)
+
+i=0
+for data in mylist:
+    if(i<5000):
+        i=i+1
+        sql="insert into HistoryGasPrice2 values('{}',{},{},{},{});".format(\
+             data[0],data[1],data[2],data[3],data[4])
+        cursor.execute(sql)
+        conn.commit()
+conn.close()
+    
+    
+    
+    
